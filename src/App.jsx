94a307ecb01150111510efd966310be4033e3496
Norma <nidaliaamtz@hotmail.com>
2022-10-24 18:37:35 -0500

    Elimine compentario de Error y Paciente
    Elimine comentario de import Error y Paciente y acomode un />

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Header from "./componentes/header.jsx"
 import ListadoPaciente from "./componentes/listadoPaciente.jsx"
 import Formulario from "./componentes/formulario.jsx"
-//import Error from "./componentes/error.jsx"
-//import Paciente from "./componentes/paciente.jsx"
+
 import { useState } from "react";
 
 function App() {
 
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
+
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter(paciente.id !== id)
+    setPacientes = { pacientesActualizados }
+
+  }
 
   return (
     <div className="container mx-auto mt-15">
@@ -19,7 +24,10 @@ function App() {
           setPacientes={setPacientes}
           paciente={paciente}
           setPaciente={setPaciente} />
-        <ListadoPaciente />
+        <ListadoPaciente
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente} />
       </div>
     </div>
   )
